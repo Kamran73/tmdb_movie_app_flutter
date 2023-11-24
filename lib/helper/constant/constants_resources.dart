acb44bdcde9244e45ce64dbf5b702617738502ca
kamran <muhammad.kamran@techswivel.com>
2023-11-24 20:00:17 +0500

    added constant_resource file

---
+++ lib/helper/constant/constants_resources.dart
@@ -0,0 +1,7 @@
+class ConstantResources {
+  ConstantResources._();
+
+  static const String MOVIES_DATA_BASE_URL = "https://api.themoviedb.org/";
+  static const String MOVIES_IMAGES_BASE_URL =
+      "https://images.tmdb.org/t/p/w500/";
+}
