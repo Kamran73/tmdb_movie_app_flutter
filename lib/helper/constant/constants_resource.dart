521dc75a24369d739b6bcfeb5b56351c5c720528
kamran <muhammad.kamran@techswivel.com>
2023-11-26 19:15:35 +0500

    tested

--- lib/helper/constant/constants_resource.dart
+++ lib/helper/constant/constants_resource.dart
@@ -1,9 +1,8 @@
 class ConstantsResource {
   ConstantsResource._();
 
-  static const String AUTH_BEARER_TOKEN =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjNiMDNmMGNjNTg4YmYwY2Q1MjJhOTNiMDhmOWYxNSIsInN1YiI6IjY1NWUxMWMxZDM5OWU2MDE1MGNiODExZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L8VxnPUo-ZSbaYxTjwFHgnCRUTD_bc3IyiQPGuVc78U";
-  static const String CLIENT_API_KEY = "fb3b03f0cc588bf0cd522a93b08f9f15";
+  static const String AUTH_BEARER_TOKEN = "YOUR-AUTHORIZATION-TOKEN-HERE";
+  static const String CLIENT_API_KEY = "YOUR-CLIENT-API-KEY-HERE";
   static const String MOVIES_DATA_BASE_URL = "https://api.themoviedb.org/";
   static const String MOVIES_IMAGES_BASE_URL =
       "https://images.tmdb.org/t/p/w500/";
